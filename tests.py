c2b249f8a15ca838c707964e10bb83f2e7f7f8a8
Gustavo R. Kuhn <gustavorichterkuhn@gmail.com>
2025-06-11 13:52:06 -0300

    Adicionado módulo de teste das funções matemáticas

---
+++ tests.py
@@ -0,0 +1,18 @@
+from math_utils import *
+
+def test_quadrado():
+    assert quadrado(2) == 4
+    assert quadrado(-3) == 9
+    
+def test_cubo():
+    assert cubo(2) == 8
+    assert cubo(-3) == -27
+    
+def test_is_int():
+    assert is_int(2) == True
+    assert is_int(2.1) == False
+    assert is_int([2]) == False
+    assert is_int([2,3]) == False
+    assert is_int("1") == False
+    assert is_int((2)) == False
+    assert is_int((2,3)) == False
